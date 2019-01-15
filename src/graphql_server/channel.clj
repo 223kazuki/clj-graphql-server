e0e8fbfe24e204a8c574842310d6876045339235
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2019-01-15 08:58:08 -0800

    pub sub.

--- src/graphql_server/channel.clj
+++ src/graphql_server/channel.clj
@@ -1,9 +1,9 @@
 (ns graphql-server.channel
-  (:require [clojure.core.async :refer [chan go-loop go >! <! timeout close!] :as async]
+  (:require [clojure.core.async :refer [chan close!]]
             [integrant.core :as ig]))
 
 (defmethod ig/init-key :graphql-server/channel [_ _]
-  (chan (async/dropping-buffer 1)))
+  (chan))
 
 (defmethod ig/halt-key! :graphql-server/channel [_ ch]
   (close! ch))
--- src/graphql_server/handler/streamer.clj
+++ src/graphql_server/handler/streamer.clj
@@ -1,24 +1,33 @@
 (ns graphql-server.handler.streamer
   (:require [integrant.core :as ig]
-            [clojure.core.async :refer [chan go-loop go >! <! timeout close!] :as async]))
+            [clojure.core.async :refer [pub sub chan go-loop go >! <! timeout close! >!! <!! unsub] :as async]))
 
 (defmethod ig/init-key ::stream-torikumis [_ {:keys [datomic ch]}]
   (fn [context args source-stream]
-    (println "start!" ch source-stream)
-    ;;(source-stream [{:id 2 :kimarite :TSUKIDASHI}])
-    (go-loop []
-      (when-let [data (<! ch)] ; チャネルを読む
-        (do
-          (println source-stream)
-          (source-stream [{:id 2 :kimarite :OSHIDASHI}])
-          (println "!!!")))
-      (recur))
-    #(println "stop!")))
+    (println "start!")
+    ;; (source-stream [{:id 2 :kimarite :TSUKIDASHI}])
+    (let [publication (pub ch :msg-type)
+          subscription (chan)]
+      (sub publication :torikumi/updated subscription)
+      (go-loop []
+        (when-let [{:keys [text]} (<! subscription)]
+          (println "!!" text)
+          (source-stream [{:id 2 :kimarite (rand-nth [:OSHIDASHI :TSUKIDASHI :UWATENAGE])}])
+          (recur)))
+      #(do
+         (println "stop!")
+         (unsub publication :torikumi/updated subscription)
+         (close! subscription)))))
 
 (comment
   (def ch (chan))
 
-  (go (>! ch "test4"))
+  (>!! (:graphql-server/channel integrant.repl.state/system)
+       {:msg-type :torikumi/updated :text "hello"})
+
+  (go (>! (:graphql-server/channel integrant.repl.state/system)
+          {:msg-type :torikumi/updated :text "hello"}))
+
   (go-loop []
     (when (>! ch "test") ; チャネルに書く
       (<! (timeout 2000)) ; 2秒待つ
@@ -32,3 +41,35 @@
       (recur)))
 
   (close! ch))
+
+(comment
+  ;; publisher is just a normal channel
+  (def publisher (chan))
+
+  ;; publication is a thing we subscribe to
+  (def publication
+    (pub publisher #(:topic %)))
+
+  (def subscriber-one (chan))
+  (def subscriber-two (chan))
+  (def subscriber-three (chan))
+
+  (sub publication :account-created subscriber-one)
+  (sub publication :account-created subscriber-two)
+  (sub publication :user-logged-in  subscriber-two)
+  (sub publication :change-page     subscriber-three)
+
+  (defn take-and-print [channel prefix]
+    (go-loop []
+      (let [data (<! channel)]
+        (println prefix ": " data)
+        (recur))))
+
+  (take-and-print subscriber-one "subscriber-one")
+  (take-and-print subscriber-two "subscriber-two")
+  (take-and-print subscriber-three "subscriber-three")
+
+  (go (>! publisher { :topic :change-page :dest "/#home" }))
+
+  (go (>! publisher { :topic :account-created :username "billy" }))
+  )
