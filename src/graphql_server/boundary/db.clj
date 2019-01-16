6c361622e4e8e2d35b59836eab6bff74276d0d02
Kazuki Tsutsumi <tsutsumi@tisus.com>
2019-01-15 19:12:42 -0800

    Move auth tables to datomic.

--- src/graphql_server/boundary/db.clj
+++ src/graphql_server/boundary/db.clj
@@ -1,6 +1,26 @@
 (ns graphql-server.boundary.db
   (:require [clojure.java.jdbc :as jdbc]
-            [duct.database.sql]))
+            [duct.database.sql]
+            [duct.database.datomic]
+            [datomic.api :as d]
+            [camel-snake-kebab.core :as cnk]
+            [camel-snake-kebab.extras :refer [transform-keys]]))
+
+(defn- ->entity [m]
+  (transform-keys cnk/->kebab-case-keyword
+                  (dissoc m :db/id)))
+
+(comment
+  (->entity
+   (d/pull (d/db (:connection (:duct.database/datomic integrant.repl.state/system)))
+           '[*]
+           [:user/email-address "rixi223.kazuki@gmail.com"]))
+  (d/q
+   '[:find ?e
+     :where [?e :user/email-address "rixi223.kazuki@gmail.com"]]
+   (d/db (:connection (:duct.database/datomic integrant.repl.state/system))))
+
+  )
 
 (defprotocol User
   (get-user-by-mail [db mail]))
@@ -12,11 +32,21 @@
   duct.database.sql.Boundary
   (get-user-by-mail [{:keys [spec]} mail]
     (let [[user] (jdbc/query spec ["SELECT * FROM \"user\" WHERE email_address = ?" mail])]
-      user)))
+      user))
+  duct.database.datomic.Boundary
+  (get-user-by-mail [{:keys [connection]} mail]
+    (-> (d/db connection)
+        (d/pull '[*] [:user/email-address mail])
+        (->entity))))
 
 (extend-protocol Client
   duct.database.sql.Boundary
   (find-client-by-id [{:keys [spec]} client-id]
     (let [[client]
           (jdbc/query spec ["SELECT * FROM client where client_id = ?" client-id])]
-      client)))
+      client))
+  duct.database.datomic.Boundary
+  (find-client-by-id [{:keys [connection]} client-id]
+    (-> (d/db connection)
+        (d/pull '[*] [:client/client-id client-id])
+        (->entity))))
