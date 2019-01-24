13716d7f65e1f2d84b974ec667825d18422981ac
Kazuki Tsutsumi <tsutsumi@tisus.com>
2019-01-23 17:05:47 -0800

    Change to create database in datomic module.

--- src/duct/database/datomic.clj
+++ src/duct/database/datomic.clj
@@ -5,5 +5,7 @@
 (defrecord Boundary [connection])
 
 (defmethod ig/init-key :duct.database/datomic [_ {:keys [connection-uri] :as options}]
+  (when (d/create-database connection-uri)
+    "database created.")
   (let [con (d/connect connection-uri)]
     (->Boundary con)))
